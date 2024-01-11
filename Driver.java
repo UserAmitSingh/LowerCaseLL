a61ebb180546688a375bf4d5416febde134ab305
UserAmitSingh <143116288+UserAmitSingh@users.noreply.github.com>
2024-01-11 12:41:05 -0600

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,5 +1,15 @@
 package Program12;
 
 public class Driver {
-
+	private static StackList sL;
+	private static QueueList qL;
+	
+	public static void main(String a[]) {
+		String[] file = {"Bill" , "Fred", "Joe", "Henrietta", "Mr. M", "Noman"};
+		//"add" the following Strings to each list in the following order:
+		//"Bill" , "Fred", "Joe", "Henrietta", "Mr. M", "Noman"
+		//printout--by using the appropriate method calls to the Lists-- the above names
+		sL = new StackList();
+		qL = new QueueList();
+	}
 }
