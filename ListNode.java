cc238fe824094501bb9ed251192022c53bfbc5d9
AYGe1 <143745172+AYGe1@users.noreply.github.com>
2024-01-17 09:05:01 -0600

    Update ListNode.java

--- ListNode.java
+++ ListNode.java
@@ -9,7 +9,9 @@ package Program12;
  * Description: Creates a ListNode class that holds an object and another
  * 		ListNode.
  */
-public class ListNode {
+public class ListNode 
+{
+	
 	//Field Variables
 	private Object element;
 	private ListNode next;
@@ -18,7 +20,8 @@ public class ListNode {
 	 * The ListNode() method is a constructor method that
 	 * instantiates the field variables element and next.
 	 */
-	public ListNode(Object e, ListNode n) {
+	public ListNode(Object e, ListNode n) 
+	{
 		element = e;
 		next = n;
 	}
