20b8ba1d4d4243a513bdb74f36856f3bb198baa0
UserAmitSingh <143116288+UserAmitSingh@users.noreply.github.com>
2024-01-17 09:04:58 -0600

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -1,5 +1,14 @@
 package Program12;
 
+/**
+ * Team : 1
+ * Members: Jackson Amick, Alex Ge, Shane Blackenship, Amit Singh
+ * Date: 1/17/2024
+ * Class: APCSA
+ * Teacher: Mr. Meinzen
+ * Description: A intercface that has that includes the methods isEmpty(),
+ * 		dequeue(), enqueue(), getBack(), getFront().
+ */
 public interface Queue {
 	public boolean isEmpty();
 	public Object dequeue();
